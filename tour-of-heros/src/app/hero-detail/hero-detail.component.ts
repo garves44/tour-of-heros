39092e097ca0b042098000edb595a08e46e593a2
Jeremy Collins <jeremycollins@utah.gov>
2020-12-03 09:04:47 -0700

    added http client to simulate getting data from a server

--- tour-of-heros/src/app/hero-detail/hero-detail.component.ts
+++ tour-of-heros/src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
@@ -11,12 +11,13 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
+  hero: Hero;
 
-  @Input() hero: Hero;
-
-  constructor(private route: ActivatedRoute,
+  constructor(
+    private route: ActivatedRoute,
     private heroService: HeroService,
-    private location: Location) { }
+    private location: Location
+  ) { }
 
   ngOnInit(): void {
     this.getHero();
@@ -32,4 +33,8 @@ export class HeroDetailComponent implements OnInit {
     this.location.back();
   }
 
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
 }
