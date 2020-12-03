6024b7eec56ae2fc075562f8813f138190af956b
Jeremy Collins <jeremycollins@utah.gov>
2020-12-03 06:56:43 -0700

    Heroes display in master/detail view, heroes can be selected to display details, conditionally render html if hero is selected, toggle css if selected

--- tour-of-heros/src/app/heroes/heroes.component.ts
+++ tour-of-heros/src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../mock-heros';
 import { Hero } from '../hero';
 
 @Component({
@@ -7,14 +8,15 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'WindStorm'
-  }
+
+  heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
