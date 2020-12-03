245e0679af441bd40b3a63b08ae0798c35562c03
Jeremy Collins <jeremycollins@utah.gov>
2020-12-03 07:47:38 -0700

    registered HeroService as provider, implemented Observables from RxJS library, created MessageService for loose coupled communication between classes

--- tour-of-heros/src/app/heroes/heroes.component.ts
+++ tour-of-heros/src/app/heroes/heroes.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { HEROES } from '../mock-heros';
+
+import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
+import { MessageService } from '../message.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -9,14 +12,23 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit() {
+    this.getHeroes()
   }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`Heroes Component: Selected hero id=${hero.id}`);
   }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
 }
